e9e8ce461d17d9b7e49fbcebb0db186e41465b4f
Brett Zamir <brettz9@yahoo.com>
2020-01-22 10:21:36 +0800

    - Enhancement: Allow `only` on test cases or CLI comma-separated `--rules` and `--valid=idxOrNegIdx`/`--invalid=idxOrNegIdx` - npm: Bump peerDeps (eslint) and devDeps (xo)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const {RuleTester} = require('eslint');
 
+const argv = require('minimist')(process.argv.slice(2));
+
 const nameGetter = () => {
   const state = new Map();
   return name => {
@@ -10,6 +12,88 @@ const nameGetter = () => {
     return n ? `${name} v${n + 1}` : name;
   };
 };
+
+class OnlyableRuleTester extends RuleTester {
+  constructor(options) {
+    const {args} = options;
+    delete options.args;
+    super(options);
+    this.options = options;
+    this.args = args || {};
+  }
+
+  run(name, ruleImpl, object) {
+    function someTestCases(cb) {
+      return ['valid', 'invalid'].some(validState => {
+        const testCases = object[validState];
+        if (!testCases) {
+          return false;
+        }
+
+        for (let i = 0; i < testCases.length; i++) {
+          const result = cb(testCases[i], i, testCases, validState);
+          if (result === 'remove') {
+            testCases.splice(i--, 1);
+          } else if (result) {
+            return true;
+          }
+        }
+
+        return false;
+      });
+    }
+
+    const {
+      rules = this.args.rules || process.env.npm_config_rules,
+      invalid = this.args.invalid || process.env.npm_config_invalid,
+      valid = this.args.valid || process.env.npm_config_valid
+    } = argv;
+    if (rules) {
+      if (!rules.split(',').includes(name)) {
+        return;
+      }
+
+      if (invalid && !valid) {
+        object.valid = [];
+      }
+
+      if (valid && !invalid) {
+        object.invalid = [];
+      }
+
+      if (someTestCases(testCase => {
+        return testCase && testCase.only;
+      })) {
+        someTestCases(testCase => {
+          if (!testCase || !testCase.only) {
+            return 'remove';
+          }
+
+          delete testCase.only; // Not expected by tester
+        });
+      } else {
+        someTestCases((testCase, idx, testCases, validState) => {
+          const indexes = valid && validState === 'valid' ?
+            String(valid).split(',') :
+            (invalid && validState === 'invalid' ?
+              String(invalid).split(',') :
+              []);
+          if (indexes.length === 0) {
+            return;
+          }
+
+          const include = indexes.includes(String(idx)) ||
+            indexes.includes(String(idx - testCases.length));
+          if (!include) {
+            return 'remove';
+          }
+        });
+      }
+    }
+
+    return super.run(name, ruleImpl, object);
+  }
+}
 
 module.exports = function (test, options) {
   let validity;
@@ -36,5 +120,5 @@ module.exports = function (test, options) {
     });
   };
 
-  return new RuleTester(options);
+  return new OnlyableRuleTester(options);
 };
