ff899440325224506564df766d89948388147d80
fisker <lionkay@gmail.com>
2024-01-06 21:55:01 +0800

    Use a example plugin instead of core rules

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
 import assert from 'node:assert';
 import test from 'ava';
-import eslintExperimentalApis from 'eslint/use-at-your-own-risk';
 import {outdent} from 'outdent';
 import AvaRuleTester from './index.js';
 
-// TODO[@fisker]: Write a simple rule instead.
-const arrowSpacing = eslintExperimentalApis.builtinRules.get('arrow-spacing');
+const examplePlugin = {
+  rules: {
+    'bad-to-good': {
+      create: (context) => ({
+        Identifier(node) {
+          if (node.name === 'bad') {
+            context.report({
+              node,
+              message: '"bad" should be named "good"',
+              fix: fixer => fixer.replaceText(node, 'good'),
+            });
+          }
+        },
+      }),
+      meta: {
+        fixable: 'code',
+      },
+    },
+  },
+};
 
 const try_ = fn => {
   try {
@@ -35,28 +52,24 @@ test('works', t => {
     onlyCalls.push([this, ...args]);
   };
 
-  const ruleTester = new AvaRuleTester(doTest, {
-    parserOptions: {
-      ecmaVersion: '2018',
-    },
-  });
+  const ruleTester = new AvaRuleTester(doTest);
 
-  ruleTester.run('eg-rule', arrowSpacing, {
+  ruleTester.run('bad-to-good', examplePlugin.rules['bad-to-good'], {
     valid: [
-      '() => {}',
-      '() => {}',
+      'good',
+      'another',
     ],
     invalid: [
       {
-        code: '()=> {}',
-        errors: ['ham'],
+        code: 'bad',
+        errors: [{message: 'incorrect error message assert'}],
       },
       {
-        code: '()=> {}',
-        errors: ['Missing space before =>.'],
-        output: 'spam',
+        code: 'bad',
+        errors: [{message: '"bad" should be named "good"'}],
+        output: 'incorrect output assert',
       },
-    ],
+    ]
   });
 
   t.deepEqual(onlyCalls, []);
@@ -82,35 +95,35 @@ test('works', t => {
   const expected = [
     {
       ctx: undefined,
-      title: 'valid(1): () => {}',
+      title: 'valid(1): good',
       result: {success: undefined},
       testCalls: [['t.pass']],
       rest: [],
     },
     {
       ctx: undefined,
-      title: 'valid(2): () => {}',
+      title: 'valid(2): another',
       result: {success: undefined},
       testCalls: [['t.pass']],
       rest: [],
     },
     {
       ctx: undefined,
-      title: 'invalid(1): ()=> {}',
+      title: 'invalid(1): bad',
       result: {
-        failure: getMessage('Missing space before =>.', 'ham'),
+        failure: getMessage('"bad" should be named "good"', 'incorrect error message assert')
       },
       testCalls: [
         [
           't.is',
-          'Missing space before =>.',
-          'ham',
+          '"bad" should be named "good"',
+          'incorrect error message assert',
           outdent`
             Expected values to be strictly equal:
             + actual - expected
 
-            + 'Missing space before =>.'
-            - 'ham'
+            + '"bad" should be named "good"'
+            - 'incorrect error message assert'
           `,
         ],
       ],
@@ -118,15 +131,15 @@ test('works', t => {
     },
     {
       ctx: undefined,
-      title: 'invalid(2): ()=> {}',
+      title: 'invalid(2): bad',
       result: {
         failure: 'Output is incorrect.',
       },
       testCalls: [
         [
           't.is',
-          '() => {}',
-          'spam',
+          'good',
+          'incorrect output assert',
           'Output is incorrect.',
         ],
       ],
@@ -148,35 +161,31 @@ test('only', t => {
     onlyCalls.push([this, ...args]);
   };
 
-  const ruleTester = new AvaRuleTester(doTest, {
-    parserOptions: {
-      ecmaVersion: '2018',
-    },
-  });
+  const ruleTester = new AvaRuleTester(doTest);
 
-  ruleTester.run('eg-rule', arrowSpacing, {
+  ruleTester.run('bad-to-good', examplePlugin.rules['bad-to-good'], {
     valid: [
-      '() => {}',
-      '() => {}',
+      'good',
+      'another',
     ],
     invalid: [
       {
-        code: '()=> {}',
-        errors: ['ham'],
+        code: 'bad',
+        errors: [{message: 'incorrect error message assert'}],
       },
       {
-        code: '()=> {}',
-        errors: ['Missing space before =>.'],
-        output: 'spam',
+        code: 'bad',
+        errors: [{message: '"bad" should be named "good"'}],
+        output: 'incorrect output assert',
         only: true,
       },
-    ],
+    ]
   });
 
   t.is(calls.length, 3);
   t.is(onlyCalls.length, 1);
   const [, title, fn] = onlyCalls[0];
-  t.is(title, 'invalid(2): ()=> {}');
+  t.is(title, 'invalid(2): bad');
 
   const testCalls = [];
   try_(() => fn({
@@ -190,8 +199,8 @@ test('only', t => {
   t.deepEqual(testCalls, [
     [
       't.is',
-      '() => {}',
-      'spam',
+      'good',
+      'incorrect output assert',
       'Output is incorrect.',
     ],
   ]);
